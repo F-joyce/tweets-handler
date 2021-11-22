9db2d86dd1504a61b04e3e2039908b89eaa4a723
F-Joyce <effedeso@gmail.com>
2021-11-22 11:40:42 +0000

    Tried new branch to solve merge issue on master

--- checker.py
+++ checker.py
@@ -1,10 +1,15 @@
 from pymongo import MongoClient
 import pandas as pd
+import pprint
 
 client = MongoClient("mongodb://127.0.0.1:27017/?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000")  # Remember your uri string
 
-col = client['tweets1']['covid']
+db = client['tweets1']
 
+col = db['covid']
 
-for doc in col.model.find({'tweet'}): 
-    print(doc) 
+_ = 0
+
+for tweet in col.find(): 
+    print(str(_) + '\n' + tweet['tweet'] + '\n') 
+    _ += 1
